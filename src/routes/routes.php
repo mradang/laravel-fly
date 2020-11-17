4ae6d02dd6d8a68202e5655c4e71b5c801823e6f
mradang <mradang@163.com>
2020-11-18 00:15:13 +0800

    使用 php 标准语法定义路由

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,28 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use mradang\LaravelFly\Controllers\LogController;
+use mradang\LaravelFly\Controllers\RbacNodeController;
+use mradang\LaravelFly\Controllers\RbacRoleController;
 
 Route::group([
     'prefix' => 'api/fly',
-    'namespace' => 'mradang\LaravelFly\Controllers',
     'middleware' => ['auth'],
 ], function () {
     Route::group(['prefix' => 'rbac'], function () {
-        Route::post('allNodes', 'RbacNodeController@all');
-        Route::post('allNodesWithRole', 'RbacNodeController@allWithRole');
-        Route::post('refreshNodes', 'RbacNodeController@refresh');
+        Route::post('allNodes', [RbacNodeController::class, 'all']);
+        Route::post('allNodesWithRole', [RbacNodeController::class, 'allWithRole']);
+        Route::post('refreshNodes', [RbacNodeController::class, 'refresh']);
 
-        Route::post('allRoles', 'RbacRoleController@all');
-        Route::post('createRole', 'RbacRoleController@create');
-        Route::post('findRoleWithNodes', 'RbacRoleController@findWithNodes');
-        Route::post('syncNodeRoles', 'RbacNodeController@syncRoles');
-        Route::post('syncRoleNodes', 'RbacRoleController@syncNodes');
-        Route::post('updateRole', 'RbacRoleController@update');
-        Route::post('deleteRole', 'RbacRoleController@delete');
-        Route::post('saveRoleSort', 'RbacRoleController@saveSort');
+        Route::post('allRoles', [RbacRoleController::class, 'all']);
+        Route::post('createRole', [RbacRoleController::class, 'create']);
+        Route::post('findRoleWithNodes', [RbacRoleController::class, 'findWithNodes']);
+        Route::post('syncNodeRoles', [RbacNodeController::class, 'syncRoles']);
+        Route::post('syncRoleNodes', [RbacRoleController::class, 'syncNodes']);
+        Route::post('updateRole', [RbacRoleController::class, 'update']);
+        Route::post('deleteRole', [RbacRoleController::class, 'delete']);
+        Route::post('saveRoleSort', [RbacRoleController::class, 'saveSort']);
     });
 
     Route::group(['prefix' => 'log'], function () {
-        Route::post('lists', 'LogController@lists');
+        Route::post('lists', [LogController::class, 'lists']);
     });
 });
