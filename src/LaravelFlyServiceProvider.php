a2598e8eaa00ac9bbf2c250a618b32aa76eeb3de
mradang <mradang@163.com>
2023-05-05 00:20:19 +0800

    增加 sql 日志频道参数

--- src/LaravelFlyServiceProvider.php
+++ src/LaravelFlyServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace mradang\LaravelFly;
 
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class LaravelFlyServiceProvider extends ServiceProvider
@@ -23,20 +25,13 @@ class LaravelFlyServiceProvider extends ServiceProvider
             ], 'deploy');
         }
 
-        $this->registerSqlLog();
         $this->registerCommands();
+        $this->registerSqlLog();
     }
 
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/fly.php', 'fly');
-    }
-
-    protected function registerSqlLog()
-    {
-        if (config('fly.sql_log')) {
-            Services\QueryLogService::log();
-        }
     }
 
     protected function registerCommands()
@@ -46,5 +41,50 @@ class LaravelFlyServiceProvider extends ServiceProvider
                 Console\MySQLDiffCommand::class,
             ]);
         }
+    }
+
+    protected function registerSqlLog()
+    {
+        if (!config('fly.sql_log')) {
+            return;
+        }
+
+        $this->app['events']->listen(QueryExecuted::class, function (QueryExecuted $query) {
+            $sqlWithPlaceholders = str_replace(['%', '?', '%s%s'], ['%%', '%s', '?'], $query->sql);
+
+            $bindings = $query->connection->prepareBindings($query->bindings);
+            $pdo = $query->connection->getPdo();
+            $realSql = $sqlWithPlaceholders;
+            $duration = $this->formatDuration($query->time / 1000);
+
+            if (count($bindings) > 0) {
+                $realSql = vsprintf($sqlWithPlaceholders, array_map([$pdo, 'quote'], $bindings));
+            }
+            Log::channel(config('fly.sql_log_channel', config('logging.default')))
+                ->debug(sprintf(
+                    '[%s: %s] [%s] %s',
+                    request()->method(),
+                    request()->getRequestUri(),
+                    $duration,
+                    $realSql,
+                ));
+        });
+    }
+
+    /**
+     * Format duration.
+     *
+     * @param  float  $seconds
+     * @return string
+     */
+    private function formatDuration($seconds)
+    {
+        if ($seconds < 0.001) {
+            return round($seconds * 1000000) . 'μs';
+        } elseif ($seconds < 1) {
+            return round($seconds * 1000, 2) . 'ms';
+        }
+
+        return round($seconds, 2) . 's';
     }
 }
