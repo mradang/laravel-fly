37d3f8b2727001f221470becd29b19c349e58cff
mradang <mradang@163.com>
2023-06-26 18:17:28 +0800

    调整发布目录结构

--- publishes/deploy/mysql.backup.download.sh
+++ publishes/deploy/mysql.backup.download.sh
@@ -17,13 +17,13 @@ source $configFile
 # 备份
 baseFileName=$KEY\_$configName\_$(date "+%Y%m%d%H%M%S")
 sqlFile=/tmp/$baseFileName.sql
-docker_exec="cd /docker/$KEY; docker-compose exec"
-ssh -p $PORT root@$HOST "$docker_exec mysql mysqldump app > $sqlFile"
+docker_exec="cd /home/$USER/$KEY/docker; docker-compose exec"
+ssh -p $PORT $USER@$HOST "$docker_exec mysql mysqldump app > $sqlFile"
 
 # 打包下载
-ssh -p $PORT root@$HOST "gzip $sqlFile"
-scp -P $PORT root@$HOST:/tmp/$baseFileName.sql.gz /d/
-ssh -p $PORT root@$HOST "rm /tmp/$baseFileName.sql.gz -f"
+ssh -p $PORT $USER@$HOST "gzip $sqlFile"
+scp -P $PORT $USER@$HOST:/tmp/$baseFileName.sql.gz /d/
+ssh -p $PORT $USER@$HOST "rm /tmp/$baseFileName.sql.gz -f"
 
 # 解压
 gunzip /d/$baseFileName.sql.gz
--- publishes/deploy/mysqldiff.remote.sh
+++ publishes/deploy/mysqldiff.remote.sh
@@ -23,12 +23,12 @@ php artisan fly:mysqlstruct >/tmp/$project.struct.json
 sed -i 's/^\(DB_DATABASE=.*\)_new$/\1/' .env
 
 # 传送库结构到宿主端app目录
-scp -P $PORT /tmp/$project.struct.json $USER@$HOST:/var/www/$KEY/serve/mysql.base_struct.json
+scp -P $PORT /tmp/$project.struct.json $USER@$HOST:/home/$USER/$KEY/www/serve/mysql.base_struct.json
 
 # 远程库比较
 docker_php="docker-compose exec php /usr/local/bin/php"
 struct_file=/var/www/html/mysql.base_struct.json
-ssh -p $PORT $USER@$HOST "cd /docker/$KEY; $docker_php artisan fly:mysqldiff --baseStructFile=$struct_file"
+ssh -p $PORT $USER@$HOST "cd /home/$USER/$KEY/docker; $docker_php artisan fly:mysqldiff --baseStructFile=$struct_file"
 
 # 清理
 rm /tmp/$project.struct.json -f
