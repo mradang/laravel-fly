7acb28eceb50b29e195f9bca7a8dbc29abd44ccb
mradang <mradang@163.com>
2023-07-28 13:49:46 +0800

    新增数据库备份脚本

---
+++ publishes/shortcut/mysql.backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rotate=90
+
+path=$(dirname $(readlink -f $0))
+
+# 指定备份目录
+backupDir=$path/storage/mysql
+[ -d $backupDir ] || mkdir -p $backupDir
+
+# 加载环境文件
+source $path/docker/.env
+
+# 备份
+backupFile=$backupDir/$COMPOSE_PROJECT_NAME\_$(date +%Y%m%d%H%M%S).sql.gz
+$path/fly exec mysql mysqldump app | gzip >$backupFile
+
+# 清理旧的备份文件
+rotate=$(($rotate + 1))
+ls -t $backupDir/*.sql.gz | tail -n +$rotate | xargs -r rm -f
